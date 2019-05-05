1cd325427bc7b400beeb75edc11b6a43f38e1fd5
MikeClark512 <mievlecl@gmail.com>
2019-05-05 13:14:47 -0600

    renamed projects and namespaces, added empty GUI project

---
+++ TelemetryGUI/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MyPubgTelemetry.GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
