dfa0be13f2b46b5c07483a14f5febcc10dae93c4
MikeClark512 <mievlecl@gmail.com>
2019-05-14 19:20:35 -0600

    GUI info message when no telemetry files are found

--- MatchAnalyzer/Program.cs
+++ MatchAnalyzer/Program.cs
@@ -1,11 +1,23 @@
-﻿namespace MyPubgTelemetry.MatchAnalyzer
+﻿using System;
+using System.Data;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyPubgTelemetry.MatchAnalyzer
 {
     class Program
     {
         static void Main(string[] args)
         {
             TelemetryApp app = TelemetryApp.App;
-
+            string jsonStr = File.ReadAllText(@"C:\Users\mclark\Desktop\player.json");
+            JToken json = JToken.Parse(jsonStr);
+            var data = json["data"];
+            var dt = data.ToObject<DataTable>();
+            
+            //DataTable dt = JsonConvert.DeserializeObject<DataTable>(json);
+            Console.WriteLine(dt);
         }
     }
 }
