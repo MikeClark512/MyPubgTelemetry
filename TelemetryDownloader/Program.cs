0926bc9f4e7d4c3d1713e186afb3608b576887c1
MikeClark512 <mievlecl@gmail.com>
2019-05-21 02:20:10 -0600

    cleanup

--- TelemetryDownloader/Program.cs
+++ TelemetryDownloader/Program.cs
@@ -1,29 +1,18 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using System.Net.Http;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
 
 namespace MyPubgTelemetry.Downloader
 {
     class Program
     {
         // Comma separated list of usernames. Case sensitive.
-        // Minimum one username, maximum six.
+        // Minimum one username. Maximum six.
         public const string USERNAMES = "wckd,Celaven,Giles333,Solunth";
 
-        void MMain(string[] args)
+        static void Main(string[] args)
         {
-            // Increase Console.ReadLine character limit
+            // Increases Console.ReadLine character limit to allow pasting the full API key.
             Console.SetIn(new StreamReader(Console.OpenStandardInput(1024), Console.InputEncoding, false, 1024));
             TelemetryApp app = TelemetryApp.App;
 
@@ -33,24 +22,20 @@ namespace MyPubgTelemetry.Downloader
             }
             Console.WriteLine("API Key Filename: " + app.DefaultApiKeyFile);
             Console.WriteLine("API Key Contents: " + app.ApiKey);
-
             TelemetryDownloader downloader = new TelemetryDownloader();
             downloader.DownloadProgressEvent += (sender, eventArgs) =>
             {
-                if (eventArgs.Rewrite)
-                    ConsoleRewrite(eventArgs.Message);
-                else
-                    Console.WriteLine(eventArgs.Message);
+                if (eventArgs.Rewrite) ConsoleRewrite(eventArgs.Message);
+                else Console.WriteLine(eventArgs.Message);
             };
             downloader.DownloadTelemetryForPlayers(USERNAMES);
-
             if (Debugger.IsAttached)
             {
                 Console.ReadKey();
             }
         }
 
-        private void ConsoleRewrite(string msg)
+        private static void ConsoleRewrite(string msg)
         {
             int remainder = Console.BufferWidth - msg.Length - 1;
             if (remainder > 0) msg += new string(' ', remainder);
@@ -64,12 +49,6 @@ namespace MyPubgTelemetry.Downloader
             string line = Console.ReadLine()?.Trim();
             File.WriteAllText(app.DefaultApiKeyFile, line);
             return line;
-        }
-
-        static void Main(string[] args)
-        {
-            Program program = new Program();
-            program.MMain(args);
         }
     }
 }
