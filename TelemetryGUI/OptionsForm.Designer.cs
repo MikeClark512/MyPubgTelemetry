d0d7e39bac6f3725878adea048c1e1f233fa78d1
MikeClark512 <mievlecl@gmail.com>
2019-05-21 02:17:05 -0600

    reorganized options panel

--- TelemetryGUI/OptionsForm.Designer.cs
+++ TelemetryGUI/OptionsForm.Designer.cs
@@ -35,7 +35,7 @@
             this.textBoxApiKey = new System.Windows.Forms.TextBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPageGeneral = new System.Windows.Forms.TabPage();
-            this.tabPagePaths = new System.Windows.Forms.TabPage();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.buttonSettingsDirOpenInFileExplorer = new System.Windows.Forms.Button();
             this.buttonDataDirOpenInFileExplorer = new System.Windows.Forms.Button();
             this.buttonDataDirBrowse = new System.Windows.Forms.Button();
@@ -43,10 +43,8 @@
             this.labelSettingsDir = new System.Windows.Forms.Label();
             this.textBoxDataDir = new System.Windows.Forms.TextBox();
             this.labelDataDir = new System.Windows.Forms.Label();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.tabControl1.SuspendLayout();
             this.tabPageGeneral.SuspendLayout();
-            this.tabPagePaths.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonCancel
@@ -75,7 +73,7 @@
             // labelApiKey
             // 
             this.labelApiKey.AutoSize = true;
-            this.labelApiKey.Location = new System.Drawing.Point(6, 3);
+            this.labelApiKey.Location = new System.Drawing.Point(6, 7);
             this.labelApiKey.Name = "labelApiKey";
             this.labelApiKey.Size = new System.Drawing.Size(70, 13);
             this.labelApiKey.TabIndex = 2;
@@ -86,10 +84,9 @@
             // 
             // textBoxApiKey
             // 
-            this.textBoxApiKey.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.textBoxApiKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxApiKey.Location = new System.Drawing.Point(9, 19);
+            this.textBoxApiKey.Location = new System.Drawing.Point(9, 23);
             this.textBoxApiKey.Multiline = true;
             this.textBoxApiKey.Name = "textBoxApiKey";
             this.textBoxApiKey.Size = new System.Drawing.Size(474, 58);
@@ -101,7 +98,6 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPageGeneral);
-            this.tabControl1.Controls.Add(this.tabPagePaths);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -110,6 +106,13 @@
             // 
             // tabPageGeneral
             // 
+            this.tabPageGeneral.Controls.Add(this.buttonSettingsDirOpenInFileExplorer);
+            this.tabPageGeneral.Controls.Add(this.buttonDataDirOpenInFileExplorer);
+            this.tabPageGeneral.Controls.Add(this.buttonDataDirBrowse);
+            this.tabPageGeneral.Controls.Add(this.textBoxSettingsDir);
+            this.tabPageGeneral.Controls.Add(this.labelSettingsDir);
+            this.tabPageGeneral.Controls.Add(this.textBoxDataDir);
+            this.tabPageGeneral.Controls.Add(this.labelDataDir);
             this.tabPageGeneral.Controls.Add(this.textBoxApiKey);
             this.tabPageGeneral.Controls.Add(this.labelApiKey);
             this.tabPageGeneral.Location = new System.Drawing.Point(4, 22);
@@ -120,51 +123,37 @@
             this.tabPageGeneral.Text = "General";
             this.tabPageGeneral.UseVisualStyleBackColor = true;
             // 
-            // tabPagePaths
-            // 
-            this.tabPagePaths.Controls.Add(this.buttonSettingsDirOpenInFileExplorer);
-            this.tabPagePaths.Controls.Add(this.buttonDataDirOpenInFileExplorer);
-            this.tabPagePaths.Controls.Add(this.buttonDataDirBrowse);
-            this.tabPagePaths.Controls.Add(this.textBoxSettingsDir);
-            this.tabPagePaths.Controls.Add(this.labelSettingsDir);
-            this.tabPagePaths.Controls.Add(this.textBoxDataDir);
-            this.tabPagePaths.Controls.Add(this.labelDataDir);
-            this.tabPagePaths.Location = new System.Drawing.Point(4, 22);
-            this.tabPagePaths.Name = "tabPagePaths";
-            this.tabPagePaths.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPagePaths.Size = new System.Drawing.Size(489, 241);
-            this.tabPagePaths.TabIndex = 1;
-            this.tabPagePaths.Text = "Paths";
-            this.tabPagePaths.UseVisualStyleBackColor = true;
-            // 
             // buttonSettingsDirOpenInFileExplorer
             // 
+            this.buttonSettingsDirOpenInFileExplorer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonSettingsDirOpenInFileExplorer.Image = global::MyPubgTelemetry.GUI.Properties.Resources.OpeninFileExplorer_16x;
-            this.buttonSettingsDirOpenInFileExplorer.Location = new System.Drawing.Point(459, 31);
+            this.buttonSettingsDirOpenInFileExplorer.Location = new System.Drawing.Point(459, 116);
             this.buttonSettingsDirOpenInFileExplorer.Name = "buttonSettingsDirOpenInFileExplorer";
             this.buttonSettingsDirOpenInFileExplorer.Size = new System.Drawing.Size(24, 22);
-            this.buttonSettingsDirOpenInFileExplorer.TabIndex = 6;
+            this.buttonSettingsDirOpenInFileExplorer.TabIndex = 13;
             this.toolTip1.SetToolTip(this.buttonSettingsDirOpenInFileExplorer, "Open in File Explorer");
             this.buttonSettingsDirOpenInFileExplorer.UseVisualStyleBackColor = true;
             this.buttonSettingsDirOpenInFileExplorer.Click += new System.EventHandler(this.ButtonSettingsDirOpenInFileExplorer_Click);
             // 
             // buttonDataDirOpenInFileExplorer
             // 
+            this.buttonDataDirOpenInFileExplorer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonDataDirOpenInFileExplorer.Image = global::MyPubgTelemetry.GUI.Properties.Resources.OpeninFileExplorer_16x;
-            this.buttonDataDirOpenInFileExplorer.Location = new System.Drawing.Point(459, 5);
+            this.buttonDataDirOpenInFileExplorer.Location = new System.Drawing.Point(459, 90);
             this.buttonDataDirOpenInFileExplorer.Name = "buttonDataDirOpenInFileExplorer";
             this.buttonDataDirOpenInFileExplorer.Size = new System.Drawing.Size(24, 22);
-            this.buttonDataDirOpenInFileExplorer.TabIndex = 5;
+            this.buttonDataDirOpenInFileExplorer.TabIndex = 12;
             this.toolTip1.SetToolTip(this.buttonDataDirOpenInFileExplorer, "Open in File Explorer");
             this.buttonDataDirOpenInFileExplorer.UseVisualStyleBackColor = true;
             this.buttonDataDirOpenInFileExplorer.Click += new System.EventHandler(this.ButtonDataDirOpenInFileExplorer_Click);
             // 
             // buttonDataDirBrowse
             // 
-            this.buttonDataDirBrowse.Location = new System.Drawing.Point(429, 5);
+            this.buttonDataDirBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDataDirBrowse.Location = new System.Drawing.Point(429, 90);
             this.buttonDataDirBrowse.Name = "buttonDataDirBrowse";
             this.buttonDataDirBrowse.Size = new System.Drawing.Size(24, 22);
-            this.buttonDataDirBrowse.TabIndex = 4;
+            this.buttonDataDirBrowse.TabIndex = 11;
             this.buttonDataDirBrowse.Text = "...";
             this.toolTip1.SetToolTip(this.buttonDataDirBrowse, "Browse...");
             this.buttonDataDirBrowse.UseVisualStyleBackColor = true;
@@ -175,20 +164,20 @@
             this.textBoxSettingsDir.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxSettingsDir.Location = new System.Drawing.Point(63, 32);
+            this.textBoxSettingsDir.Location = new System.Drawing.Point(74, 117);
             this.textBoxSettingsDir.Name = "textBoxSettingsDir";
             this.textBoxSettingsDir.ReadOnly = true;
-            this.textBoxSettingsDir.Size = new System.Drawing.Size(360, 20);
-            this.textBoxSettingsDir.TabIndex = 3;
+            this.textBoxSettingsDir.Size = new System.Drawing.Size(349, 20);
+            this.textBoxSettingsDir.TabIndex = 10;
             // 
             // labelSettingsDir
             // 
             this.labelSettingsDir.AutoSize = true;
-            this.labelSettingsDir.Location = new System.Drawing.Point(6, 35);
+            this.labelSettingsDir.Location = new System.Drawing.Point(6, 120);
             this.labelSettingsDir.Name = "labelSettingsDir";
-            this.labelSettingsDir.Size = new System.Drawing.Size(51, 13);
-            this.labelSettingsDir.TabIndex = 2;
-            this.labelSettingsDir.Text = "Settings: ";
+            this.labelSettingsDir.Size = new System.Drawing.Size(48, 13);
+            this.labelSettingsDir.TabIndex = 9;
+            this.labelSettingsDir.Text = "Settings:";
             this.toolTip1.SetToolTip(this.labelSettingsDir, "Most application preferences are stored in this file.");
             // 
             // textBoxDataDir
@@ -196,19 +185,19 @@
             this.textBoxDataDir.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxDataDir.Location = new System.Drawing.Point(63, 6);
+            this.textBoxDataDir.Location = new System.Drawing.Point(74, 91);
             this.textBoxDataDir.Name = "textBoxDataDir";
-            this.textBoxDataDir.Size = new System.Drawing.Size(360, 20);
-            this.textBoxDataDir.TabIndex = 1;
+            this.textBoxDataDir.Size = new System.Drawing.Size(349, 20);
+            this.textBoxDataDir.TabIndex = 8;
             // 
             // labelDataDir
             // 
             this.labelDataDir.AutoSize = true;
-            this.labelDataDir.Location = new System.Drawing.Point(6, 9);
+            this.labelDataDir.Location = new System.Drawing.Point(6, 94);
             this.labelDataDir.Name = "labelDataDir";
-            this.labelDataDir.Size = new System.Drawing.Size(33, 13);
-            this.labelDataDir.TabIndex = 0;
-            this.labelDataDir.Text = "Data:";
+            this.labelDataDir.Size = new System.Drawing.Size(62, 13);
+            this.labelDataDir.TabIndex = 7;
+            this.labelDataDir.Text = "Data folder:";
             this.toolTip1.SetToolTip(this.labelDataDir, "Telemetry data files will be stored and and read from this directory.");
             // 
             // OptionsForm
@@ -229,8 +218,6 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPageGeneral.ResumeLayout(false);
             this.tabPageGeneral.PerformLayout();
-            this.tabPagePaths.ResumeLayout(false);
-            this.tabPagePaths.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -242,15 +229,14 @@
         private System.Windows.Forms.TextBox textBoxApiKey;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPageGeneral;
-        private System.Windows.Forms.TabPage tabPagePaths;
-        private System.Windows.Forms.Label labelDataDir;
-        private System.Windows.Forms.TextBox textBoxDataDir;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.LinkLabel labelApiKey;
+        private System.Windows.Forms.Button buttonSettingsDirOpenInFileExplorer;
+        private System.Windows.Forms.Button buttonDataDirOpenInFileExplorer;
+        private System.Windows.Forms.Button buttonDataDirBrowse;
         private System.Windows.Forms.TextBox textBoxSettingsDir;
         private System.Windows.Forms.Label labelSettingsDir;
-        private System.Windows.Forms.Button buttonDataDirBrowse;
-        private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button buttonDataDirOpenInFileExplorer;
-        private System.Windows.Forms.Button buttonSettingsDirOpenInFileExplorer;
-        private System.Windows.Forms.LinkLabel labelApiKey;
+        private System.Windows.Forms.TextBox textBoxDataDir;
+        private System.Windows.Forms.Label labelDataDir;
     }
 }
